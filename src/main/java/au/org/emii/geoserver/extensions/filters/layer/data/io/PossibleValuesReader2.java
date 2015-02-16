0c45b99e69ac7184cd50f292f66fbd2a947ae8f7
julian1 <julian.atkinson71@gmail.com>
2015-02-16 11:51:43 +1100

    ugly conversion

--- src/main/java/au/org/emii/geoserver/extensions/filters/layer/data/io/PossibleValuesReader2.java
+++ src/main/java/au/org/emii/geoserver/extensions/filters/layer/data/io/PossibleValuesReader2.java
@@ -32,7 +32,22 @@ import java.sql.SQLException;
 import java.lang.IllegalAccessException;
 import java.lang.reflect.InvocationTargetException;
 
+import java.lang.reflect.Type;
+
 public class PossibleValuesReader2 {
+
+	private static final String TYPE_NAME_PREFIX = "class ";
+ 
+	public static String getClassName(Type type) {
+		if (type==null) {
+			return "";
+		}
+		String className = type.toString();
+		if (className.startsWith(TYPE_NAME_PREFIX)) {
+			className = className.substring(TYPE_NAME_PREFIX.length());
+		}
+		return className;
+	}
 
     public Set<String> read(DataStoreInfo dataStoreInfo, LayerInfo layerInfo, String propertyName )
         throws IOException, NoSuchMethodException, SQLException, IllegalAccessException, InvocationTargetException
@@ -64,6 +79,61 @@ public class PossibleValuesReader2 {
         storeGetAggregateValueMethod.invoke(store, visitor, schema, query, conn );
 
         Set result = visitor.getUnique();
+
+        // we need to map to string form here, which means that we need the actual type ...
+        for( Object item : result ) 
+        {
+			// String name1 = item.class.name ; 
+			//String name2 = item.getClass().getSimpleName(); 
+
+			String name2 = item.getClass().toString(); 
+	
+			//*/String x = String.class; 
+
+
+			Object value = item; 
+		
+			if (value.getClass() == Integer.class) {
+				System.out.println("This is an Integer");
+			} 
+			else if (value.getClass() == String.class) {
+				System.out.println("This is a String");
+			}
+			else if (value.getClass() == Float.class) {
+				System.out.println("This is a Float");
+			}	
+
+/*
+			if (item instanceof String.class) {
+				System.out.println( "it's a string " );
+			}
+			if (item instanceof Long.class) {
+				System.out.println( "it's a long" );
+			}
+*/
+
+
+
+			/* 
+			//String name2 = item.getClass().getName(); 
+			no string switch support...
+			switch( name ){
+				 case "A":	
+					System.out.println( "hi" );
+			};
+			// CLAZZ z = CLAZZ.valueOf( item.getClass().getSimpleName());
+			*/
+
+			if (name2.equals(String.class)) {
+				System.out.println( "hi" );
+			} 
+			if (name2.equals(Long.class)) {
+				System.out.println( "hi" );
+			} 
+
+
+
+        } 
 
         Set<String> result2 = new TreeSet<String>( result );
 
