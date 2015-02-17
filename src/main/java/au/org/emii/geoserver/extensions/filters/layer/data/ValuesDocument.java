84796251456a1d9dde0596847b00511aac330e31
julian1 <julian.atkinson71@gmail.com>
2015-02-17 15:54:20 +1100

    tidy

--- src/main/java/au/org/emii/geoserver/extensions/filters/layer/data/ValuesDocument.java
+++ src/main/java/au/org/emii/geoserver/extensions/filters/layer/data/ValuesDocument.java
@@ -22,20 +22,14 @@ import java.text.SimpleDateFormat;
 import javax.sql.DataSource;
 import java.util.*;
 
-
-
-
 public class ValuesDocument {
-
-
-
 
     public Document build(Set values) throws Exception {
         Document document = getNewDocument();
         Element valuesElement = document.createElement( "uniqueValues" );
-        document.appendChild( valuesElement);
+        document.appendChild(valuesElement);
 
-        for (String value : convert(values)) {
+        for (String value : encodeValues(values)) {
               Element element = document.createElement( "value" );
               element.appendChild(document.createTextNode(value));
               valuesElement.appendChild(element);
@@ -50,82 +44,55 @@ public class ValuesDocument {
         return docBuilder.newDocument();
     }
 
+    private List<String> encodeValues(Set values) {
 
-    private List<String> convert(Set result) {
+        List<String> result = new ArrayList<String>();
 
-        // Set result = new TreeSet(visitor.getUnique());
-        // all elts are guaranteed to be the same type
-        Class clazz = result.iterator().next().getClass();
+        if(!values.isEmpty()) {
 
-        // list stuff should probably be done near the document formatter, since it's an output type.
-        List<String> result2 = new ArrayList<String>();
+            Class clazz = values.iterator().next().getClass();
 
-        if (clazz.equals(Boolean.class)) {
-            for(Object value : result) {
-                result2.add(Boolean.toString((Boolean)value));
+            if (clazz.equals(Boolean.class)) {
+                for(Object value : values) {
+                    result.add(Boolean.toString((Boolean)value));
+                }
             }
-        }
-        else if (clazz.equals(Integer.class)) {
-            for(Object value : result) {
-                result2.add(Integer.toString((Integer)value));
+            else if (clazz.equals(Integer.class)) {
+                for(Object value : values) {
+                    result.add(Integer.toString((Integer)value));
+                }
             }
-        }
-        else if (clazz.equals(Long.class)) {
-            for(Object value : result) {
-                result2.add(Long.toString((Long)value));
+            else if (clazz.equals(Long.class)) {
+                for(Object value : values) {
+                    result.add(Long.toString((Long)value));
+                }
             }
-        }
-        else if (clazz.equals(Float.class)) {
-            for(Object value : result) {
-                result2.add(Float.toString((Float)value));
+            else if (clazz.equals(Float.class)) {
+                for(Object value : values) {
+                    result.add(Float.toString((Float)value));
+                }
             }
-        }
-        else if (clazz.equals(Double.class)) {
-            for(Object value : result) {
-                result2.add(Double.toString((Double)value));
+            else if (clazz.equals(Double.class)) {
+                for(Object value : values) {
+                    result.add(Double.toString((Double)value));
+                }
             }
-        }
-        else if (clazz.equals(String.class)) {
-            for(Object value : result) {
-                result2.add((String)value);
+            else if (clazz.equals(String.class)) {
+                for(Object value : values) {
+                    result.add((String)value);
+                }
             }
-        }
-        else if (clazz.equals(java.sql.Date.class)) {
-            SimpleDateFormat df = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss'Z'");
-            for(Object value : result) {
-                result2.add(df.format((Date)value ));
+            else if (clazz.equals(java.sql.Date.class)) {
+                SimpleDateFormat df = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss'Z'");
+                for(Object value : values) {
+                    result.add(df.format((Date)value ));
+                }
             }
-        }
-        else {
-           throw new RuntimeException("Unrecognized type" );
+            else {
+               throw new RuntimeException("Unrecognized type" );
+            }
         }
 
-        return result2;
+        return result;
     }
-
- 
-/*
-    private Document getNewDocument() throws ParserConfigurationException {
-        DocumentBuilderFactory docFactory = DocumentBuilderFactory.newInstance();
-        DocumentBuilder docBuilder = docFactory.newDocumentBuilder();
-
-        return docBuilder.newDocument();
-    }
-
-    private void appendFilter(Document document, Node filtersElement, Filter filter) {
-        Element filterElement = appendChild(document, filtersElement, "filter");
-
-        appendChild(document, filterElement, "name").appendChild(document.createTextNode(filter.getName()));
-        appendChild(document, filterElement, "type").appendChild(document.createTextNode(filter.getType()));
-        appendChild(document, filterElement, "label").appendChild(document.createTextNode(filter.getLabel()));
-        appendChild(document, filterElement, "visualised").appendChild(document.createTextNode(filter.getVisualised().toString()));
-    }
-
-    private Element appendChild(Document document, Node parent, String name) {
-        Element element = document.createElement(name);
-        parent.appendChild(element);
-
-        return element;
-    }
-*/
 }
